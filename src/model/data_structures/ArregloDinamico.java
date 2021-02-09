9cb062cf1d84d9089eb48706c116b4cfef95c4b4
AndresCastaneda1 <69652201+AndresCastaneda1@users.noreply.github.com>
2021-02-09 18:27:30 -0500

    Modificaciones a la clase Arreglo dinamico

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -7,68 +7,78 @@ package model.data_structures;
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico implements IArregloDinamico {
-		/**
-		 * Capacidad maxima del arreglo
-		 */
-        private int tamanoMax;
-		/**
-		 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
-		 */
-        private int tamanoAct;
-        /**
-         * Arreglo de elementos de tamaNo maximo
-         */
-        private String elementos[ ];
+public  class ArregloDinamico<T extends Comparable<T>> implements IArregloDinamico<T> {
+	/**
+	 * Capacidad maxima del arreglo
+	 */
+	private int tamanoMax;
+	/**
+	 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
+	 */
+	private int tamanoAct;
+	/**
+	 * Arreglo de elementos de tamaNo maximo
+	 */
+	private T elementos[ ];
 
-        /**
-         * Construir un arreglo con la capacidad maxima inicial.
-         * @param max Capacidad maxima inicial
-         */
-		public ArregloDinamico( int max )
-        {
-               elementos = new String[max];
-               tamanoMax = max;
-               tamanoAct = 0;
-        }
-        
-		public void agregar( String dato )
-        {
-               if ( tamanoAct == tamanoMax )
-               {  // caso de arreglo lleno (aumentar tamaNo)
-                    tamanoMax = 2 * tamanoMax;
-                    String [ ] copia = elementos;
-                    elementos = new String[tamanoMax];
-                    for ( int i = 0; i < tamanoAct; i++)
-                    {
-                     	 elementos[i] = copia[i];
-                    } 
-            	    System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
-               }	
-               elementos[tamanoAct] = dato;
-               tamanoAct++;
-       }
-
-		public int darCapacidad() {
-			return tamanoMax;
-		}
-
-		public int darTamano() {
-			return tamanoAct;
-		}
-
-		public String darElemento(int i)
+	/**
+	 * Construir un arreglo con la capacidad maxima inicial.
+	 * @param max Capacidad maxima inicial
+	 */
+	public ArregloDinamico( int max )
+	{
+		elementos = (T[]) new Comparable[max];
+		tamanoMax = max;
+		tamanoAct = 0;
+	}
+	
+	public String toString()
+	{
+		String respuesta ="";
+		int i=0;
+		while(i<tamanoAct)
 		{
-			String respuesta="";
-		respuesta += elementos[i];
+			respuesta+=elementos[i]+"\n";
+			i++;
+		}
 		return respuesta;
-			
-		}
+	}
+	public int darCapacidad() {
+		return tamanoMax;
+	}
 
-		public String buscar(String dato)
-		{
-			
-		String respuesta="el string dato no existe dentro del arreglo dinamico";
+	public int darTamano() {
+		return tamanoAct;
+	}
+
+	public T darElemento(int i) {
+		 
+		return elementos[i];
+	}
+
+	@Override
+	public void agregar( T dato )
+	{
+		
+				if ( tamanoAct == tamanoMax )
+				{ 
+					tamanoMax = 2 * tamanoMax;
+					T [ ] copia = elementos;
+					elementos = (T[])new Comparable[tamanoMax];
+					for ( int i = 0; i < tamanoAct; i++)
+					{
+						elementos[i] = copia[i];
+					} 
+					System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
+				}	
+				elementos[tamanoAct] = dato;
+				tamanoAct++;
+	}
+	@Override
+	public T buscar(T dato) {
+
+	
+		T respuesta=null;
 		int i =0;
 		boolean x = false;
 		while(i<tamanoAct && !x)
@@ -81,20 +91,20 @@ public class ArregloDinamico implements IArregloDinamico {
 			i++;
 		}
 		return respuesta;
-		}
 
-		public String eliminar(String dato)
-		{
-			
-		String buscado="No existe el elemento a eliminar";
+	}
+	@Override
+	public T eliminar(T dato) {
+
+		T buscado=null;
 		int i =0;
-		boolean x = false;
-		while(i<tamanoAct && !x)
+		boolean centinel = false;
+		while(i<tamanoAct && !centinel)
 		{
 			if(elementos[i].compareTo(dato)==0)
 			{
 				buscado= elementos[i];
-				x=true;
+				centinel=true;
 
 			}
 			i++;
@@ -106,7 +116,24 @@ public class ArregloDinamico implements IArregloDinamico {
 		}
 		tamanoAct--;
 		return buscado;
+	}
+	public void invertirElementos()
+	{
+		T[]temp= (T[])new Object[tamanoMax];
+		for(int i =tamanoAct; i>=0;i--)
+		{
+			temp[i]= elementos[i];
 			
 		}
+		
+	}
+
+	@Override
+	public int compareTo(T obj)
+	{
+		return 0;
+	}
+
+
 
 }
